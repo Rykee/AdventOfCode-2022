60acfe2bb4f27a4402cc418ba1851e2caa41bf51
Rykee <matrayz@gmail.com>
2022-12-01 06:09:12 +0100

    Day 1

--- src/main/java/hu/rhykee/Main.java
+++ src/main/java/hu/rhykee/Main.java
@@ -12,7 +12,7 @@ public class Main {
 
     public static void main(String[] args) {
         String cookie = args[0];
-        for (int i = 1; i <= 18; i++) {
+        for (int i = 1; i <= 1; i++) {
             Challenge solverByDay = ReflectionUtils.getSolverByDay(i);
             List<String> input = getInput(i, cookie);
             solverByDay.part1(input);
--- src/main/java/hu/rhykee/solver/task01/Task01Solver.java
+++ src/main/java/hu/rhykee/solver/task01/Task01Solver.java
@@ -2,16 +2,36 @@ package hu.rhykee.solver.task01;
 
 import hu.rhykee.solver.Challenge;
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 public class Task01Solver implements Challenge {
     @Override
     public void part1(List<String> lines) {
+        String all = String.join("\n", lines);
+        Optional<Long> max = Arrays.stream(all.split("\n\n"))
+                .map(s -> Arrays.stream(s.split("\n"))
+                        .map(Long::parseLong)
+                        .reduce(Long::sum)
+                        .get())
+                .max(Long::compareTo);
+        System.out.println(max.get());
 
     }
 
     @Override
     public void part2(List<String> lines) {
-
+        String all = String.join("\n", lines);
+        List<Long> calories = Arrays.stream(all.split("\n\n"))
+                .map(s -> Arrays.stream(s.split("\n"))
+                        .map(Long::parseLong)
+                        .reduce(Long::sum)
+                        .get())
+                .sorted(Long::compareTo)
+                .toList();
+        int lastIndex = calories.size() - 1;
+        System.out.println(calories.get(lastIndex)+calories.get(lastIndex-1)+calories.get(lastIndex-2));
     }
 }
